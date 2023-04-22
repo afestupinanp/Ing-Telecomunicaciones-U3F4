a5f340b6f2a613ad982d905fda68f08c0e941c73
Andrés Felipe Estupiñán Peláez <afestupinanp@unadvirtual.edu.co>
2023-04-21 22:44:45 -0500

    Publish latest

---
+++ assets/ComercioElectronico-9d48594c.js
@@ -0,0 +1,1 @@
+import{S as o,i as s,s as t}from"./index-559c58c6.js";class a extends o{constructor(e){super(),s(this,e,null,null,t,{})}}export{a as default};
---
+++ assets/Home-cbd02b52.js
@@ -0,0 +1,8 @@
+import{S as t,i as o,s as r,e as c,a as l,b as d,n as s,d as u,o as m}from"./index-559c58c6.js";function p(n){let e;return{c(){e=c("div"),e.innerHTML=`<div class="col-12"><h4>Cordial saludo compañeros y tutor</h4> 
+        <p>En esta página se comparten las temáticas relacionadas a la Unidad 3 - Fase 4: Implementar las soluciones de telecomunicaciones planteadas. Un saludo a todos.</p></div> 
+    <div class="col-12"><h4>Datos</h4> 
+        <p>Andrés Felipe Estupiñán Peláez<br/>
+            Ingeniería de sistemas<br/>
+            Universidad Nacional Abierta y a Distancia<br/>
+            Grupo 22, Ingeniería de las Telecomunicaciones<br/>
+            Tutor: Harold Emilio Cabrera Meza</p></div>`,l(e,"class","row")},m(a,i){d(a,e,i)},p:s,i:s,o:s,d(a){a&&u(e)}}}function b(n){return m(()=>{document.querySelector("title").innerHTML="Tema 2: Teleasistencia, teletrabajo, telemedicina, teleeducación y comercio electrónico - Inicio"}),[]}class v extends t{constructor(e){super(),o(this,e,b,p,r,{})}}export{v as default};
